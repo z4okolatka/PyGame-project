d19a445dec80d4f9c4ae7a266153ec09c1081293
Andrey <4okolatka228@gmail.com>
2022-12-18 19:20:41 +0400

    pixel perfect jump

--- src/classes/player.py
+++ src/classes/player.py
@@ -33,8 +33,8 @@ class Player(pg.sprite.Sprite, coordHelper.FloatCords):
         self.vx_acceleration = settings.PLAYER_HORIZONTAL_ACCELERATION
 
         self.max_vy = settings.PLAYER_MAX_VERTICAL_SPEED
-        self.gravity = settings.PLAYER_VERTICAL_ACCELERATION
-        self.jump_speed = settings.PLAYER_JUMP_SPEED
+        self.gravity = settings.PLAYER_FALL_ACCELERATION
+        self.jump_initial_speed = settings.PLAYER_JUMP_INITIAL_SPEED
         self.max_jump_num = settings.PLAYER_MAX_JUMP_NUM
         self.jump_num = self.max_jump_num
 
@@ -67,24 +67,24 @@ class Player(pg.sprite.Sprite, coordHelper.FloatCords):
         if self.keydown(pg.K_d, keyboard) or self.keydown(pg.K_a, keyboard):
             self.vx = 0
         if keyboard[pg.K_d]:
-            self.vx += self.vx_acceleration * self.game.deltatime
+            self.vx += self.vx_acceleration
         if keyboard[pg.K_a]:
-            self.vx -= self.vx_acceleration * self.game.deltatime
+            self.vx -= self.vx_acceleration
         if not keyboard[pg.K_a] and not keyboard[pg.K_d] and self.vx:
             if self.vx < 0:
-                self.vx += self.vx_acceleration * self.game.deltatime
+                self.vx += self.vx_acceleration
             else:
-                self.vx -= self.vx_acceleration * self.game.deltatime
-            if abs(self.vx) < 1:
+                self.vx -= self.vx_acceleration
+            if abs(self.vx) <= self.vx_acceleration:
                 self.vx = 0
 
     def normalize_horizontal_speed(self):
-        if abs(self.vx) >= self.max_vx * self.game.deltatime:
+        if abs(self.vx) >= self.max_vx:
             self.vx /= abs(self.vx)
-            self.vx *= self.max_vx * self.game.deltatime
+            self.vx *= self.max_vx
 
     def move_x(self):
-        self.x += self.vx
+        self.x += self.vx * self.game.deltatime
         self.rect.x = round(self.x)
 
     def horizontal_collision(self):
@@ -93,31 +93,27 @@ class Player(pg.sprite.Sprite, coordHelper.FloatCords):
                 if self.rect.centerx <= sprite.rect.centerx and self.vx > 0:
                     self.right = sprite.rect.left
                     self.vx = 0
-                    break
                 elif self.rect.centerx > sprite.rect.centerx and self.vx < 0:
                     self.left = sprite.rect.right
                     self.vx = 0
-                    break
+                break
 
         self.rect.x = round(self.x)
 
     def vertical_movement(self, keyboard):
         if self.keydown(pg.K_SPACE, keyboard) and self.jump_num:
-            self._jump_height = self.y
-            self.vy = self.jump_speed * -1 * self.game.deltatime
+            self.vy = -self.jump_initial_speed
             self.jump_num -= 1
         self.vy += self.gravity * self.game.deltatime
 
     def normalize_vertical_speed(self):
-        if abs(self.vy) >= self.max_vy * self.game.deltatime:
+        if abs(self.vy) >= self.max_vy:
             self.vy /= abs(self.vy)
-            self.vy *= self.max_vy * self.game.deltatime
+            self.vy *= self.max_vy
 
     def move_y(self):
-        self.y += self.vy
-        if self.vy > 0 and self._jump_height:
-            # print(self.y - self._jump_height)
-            self._jump_height = 0
+        self.y += self.vy * self.game.deltatime
+        self.vy += self.gravity * self.game.deltatime
         self.rect.y = round(self.y)
 
     def vertical_collision(self):
@@ -127,11 +123,10 @@ class Player(pg.sprite.Sprite, coordHelper.FloatCords):
                     self.vy = 0
                     self.bottom = sprite.rect.top
                     self.jump_num = self.max_jump_num
-                    break
                 elif self.rect.centery > sprite.rect.centery and self.vy < 0:
                     self.vy = 0
                     self.top = sprite.rect.bottom
-                    break
+                break
 
         self.rect.y = round(self.y)
 
